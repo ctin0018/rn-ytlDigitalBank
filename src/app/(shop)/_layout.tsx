e083f554bde0ca924098346d0517c5dffbe94531
ctin0018 <127030452+ctin0018@users.noreply.github.com>
2024-10-22 21:09:34 +0800

    Master Layout in Expo Router

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,12 +1,55 @@
 import { Tabs } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+function TabBarIcon(props: {
+    name: React.ComponentProps<typeof FontAwesome>['name'];
+    color: string;
+}) {
+    return <FontAwesome size={24} {...props} style={{ color: '#1BC464' }} />;
+}
 
 const TabsLayout = () => {
     return (
-        <Tabs>
-            <Tabs.Screen name="index" options={{}} />
-            <Tabs.Screen name="orders" options={{}} />
-        </Tabs>
+        <SafeAreaView edges={['top']} style={styles.safeArea}>
+            <Tabs
+                screenOptions={{
+                    tabBarActiveTintColor: '#1BC464',
+                    tabBarInactiveTintColor: 'grey',
+                    tabBarLabelStyle: { fontSize: 16 },
+                    tabBarStyle: {
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
+                        paddingTop: 10,
+                    },
+                    headerShown: false,
+                }}
+            >
+                <Tabs.Screen 
+                name="index" 
+                options={{
+                    title: 'shop',
+                    tabBarIcon(props) {
+                        return <TabBarIcon name="shopping-cart" {...props} />;
+                    }
+                }} />
+                <Tabs.Screen name="orders" 
+                options={{
+                    title: 'orders',
+                    tabBarIcon(props) {
+                        return <TabBarIcon name="book" {...props} />;
+                    }
+                }} />
+            </Tabs>
+        </SafeAreaView>
     );
     }
 
 export default TabsLayout;
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    },
+});
